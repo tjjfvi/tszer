1fe0c49789041fdf809cc08aa8abd33b914a20fc
tjjfvi <tjjfvi@tjjfvi.com>
2020-06-07 12:22:16 -0700

    added build scripts

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+
+// @create-index {"mode":"*"}
+
+export * from './Serializer';
+export * from './array';
+export * from './buffer';
+export * from './concat';
+export * from './constant';
+export * from './double';
+export * from './flags';
+export * from './float';
+export * from './int';
+export * from './optionalBank';
+export * from './stl';
+export * from './string';
+export * from './uint';
+
