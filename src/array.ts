0d4524dfc7582b30ecc2f8e0dfb688bb5bf36aed
tjjfvi <tjjfvi@tjjfvi.com>
2020-06-09 18:20:42 -0700

    fix stack overflow error in repeated/array

--- src/array.ts
+++ src/array.ts
@@ -1,25 +1,9 @@
 import { Serializer } from "./Serializer";
-import { concat } from "./concat";
+import { concat, _concat } from "./concat";
 import { uint16LE } from "./uint";
 
 export const repeated = <T>(t: Serializer<T>, length: number): Serializer<T[]> => {
-  if (length === 1)
-    return t.map({
-      serialize: (arr) => {
-        if (arr.length !== 1)
-          throw new Error("repeated was passed invalid array of length !== 1");
-        return arr[0];
-      },
-      deserialize: t => [t],
-    });
-  return concat(t, repeated(t, length - 1)).map({
-    serialize: (arr) => {
-      if (arr.length !== length)
-        throw new Error(`repeated was passed invalid array of length !== ${length}`);
-      return [arr[0], arr.slice(1)];
-    },
-    deserialize: ([val, arr]) => [val, ...arr],
-  });
+  return _concat(...Array(length).fill(t));
 }
 
 export const array = <T>(t: Serializer<T>, numberSerializer = uint16LE()) =>
