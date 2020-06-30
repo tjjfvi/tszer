58908a18cfba6065f73c12739e011ff15db9bac5
tjjfvi <tjjfvi@tjjfvi.com>
2020-06-30 15:23:55 -0700

    added buffer parameter to constant

--- src/constant.ts
+++ src/constant.ts
@@ -1,7 +1,6 @@
 import { Serializer } from "./Serializer";
 
-export const constant = <T>(value: T) => new Serializer<T>({
-  length: 0,
-  serialize: () => { },
-  deserialize: () => value,
+export const constant = <T>(value: T, buffer = Buffer.alloc(0)) => new Serializer<T>({
+  serialize: (_, writeChunk) => writeChunk(buffer),
+  deserialize: async () => value,
 });
